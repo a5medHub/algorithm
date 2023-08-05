abad47840c02554a778630c6d34474bcc418c263
ayassine <ayassine.auce@gmail.com>
2023-08-05 21:12:07 +0300

    fix bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let val = 3;
 let k = 0;
 let arr = [];
 // var removeElement = function (nums, val) {
-function removeElement (nums, val){
+function removeElement(nums, val){
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] != val) {
       k += 1;
